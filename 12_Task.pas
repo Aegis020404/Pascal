25aa4538655b0e554001f1dcf902033e660f6972
Aegis020404 <40359399+Aegis020404@users.noreply.github.com>
2021-02-08 10:13:02 +0300

    Add files via upload
    PascalTask

---
+++ 12_Task.pas
@@ -0,0 +1,21 @@
+﻿Program task_12;
+var a,b,a1,a2,z,x,y:real;
+
+begin
+  writeln('Введите число x');
+  readln(x);//:=2.444;
+  writeln('Введите число y');
+  readln(y);//=0.869;
+  writeln('Введите число z');
+  readln(z);//:=-0.166;
+  a1:=exp(x*LN(y+1))*exp(e*LN(y-1));
+  a2:=exp(e*LN(1+x))*abs(y - tan(z));
+  a:=a1/a2;
+  writeln('число а = ',a:2:2);
+  
+  b:=LN(cos(x+1))+sqr(abs((y-x)/2))+sqr(sin(z))/ln(z);
+  
+  writeln('число b = ',b:2:2);
+  
+
+end.
---
+++ 12_Task2.pas
@@ -0,0 +1,22 @@
+﻿Program _task_A;
+var y,x,a,b:real;
+begin
+  writeln('Введите число х');
+  readln(x);
+   writeln('Введите число a');
+  readln(a);
+    writeln('Введите число b');
+  readln(b);
+  if x>1/2 then begin
+   y:=arccos(1/sqrt(1+2*x));
+   writeln('x>1/2, y = ',y:8:5);
+  end
+  else if x>=0 then begin
+    y:= (sin(x)/cos(x) - cos(x - ln(x))/sin(x - ln(x)))/abs(sqr(x)+a*x+b);
+    writeln('x=>0, y = ',y:8:5);
+  end
+  else begin
+    writeln('y = undefined');
+  end;
+  
+end.
